e6db154b172b9b6f0942ab362bb57b66cd16eb05
iohanalopes <111798386+iohanalopes@users.noreply.github.com>
2024-09-24 20:30:54 -0300

    Add files via upload

---
+++ poo/grafite/adapter.ts
@@ -0,0 +1,19 @@
+class Adapter {
+    constructor(thickness: number) {
+    }
+
+    insert(thickness: number, hardness: string, length: number) {
+    }
+
+    remove() {
+    }
+
+    writePage() {
+    }
+
+    toString() {
+        return "";
+    }
+}
+
+export {Adapter};
