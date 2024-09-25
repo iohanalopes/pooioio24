b84cfba22679144b1b49e1b9431e1b7fcfd8975d
iohanalopes <111798386+iohanalopes@users.noreply.github.com>
2024-09-24 22:27:42 -0300

    Update grafite.ts

--- poo/grafite/grafite.ts
+++ poo/grafite/grafite.ts
@@ -1,25 +1,36 @@
-class Grafite {
-  private thickness: float; //calibre, espessura
-  private hardness: string;  //dureza
-  private size: int; //tamanho
-
-  public constructor(thickness: float, hardness: string, size: int) {
-    this.thickness = thickness;
-    this.hardness = hardness;
-    this.size = size;
-  }
-
-  public usagePerSheet(): number {
-      if (this.hardness === 'HB')
-          return 1;
-      if (this.hardness === '2B')
-          return 2;
-      if (this.hardness === '4B')
-          return 4;
-      if (this.hardness === '6B')
-          return 6;
-      return 0;
-  }
-
+class Grafite { // Lead
+    private size: number; // Tamho do grafite (50)
+    private thickness: number; // Espessura do grafite (0.5)
+    private hardness: string; // Dureza do grafite (2B)
   
-}
+    constructor(size: number, thickness: number, hardness: string){
+      this.size = size;
+      this.thickness = thickness;
+      this.hardness = hardness;
+    }
+  
+    // Retorna a quantidade de grafito gasto pro folha.
+    public usagePerSheet(): number {
+      if(this.hardness === "HB") return 1;
+      if(this.hardness === "2B") return 2;
+      if(this.hardness === "4B") return 4;
+      if(this.hardness === "6B") return 6;
+  
+      return 0;
+    }
+  
+    // Getters
+    public getSize(): number{ return this.size; }
+    public getThickness(): number{ return this.thickness; }
+    public getHardness(): string{ return this.hardness; }
+  
+    // Setter
+    public setSize(size: number){ this.size = size; }
+  
+    // Retorna as propriedades do Grafite em string
+    public toString(){
+      return `[${this.thickness}:${this.hardness}:${this.size}]`
+    }
+  }
+  
+  export { Grafite };
