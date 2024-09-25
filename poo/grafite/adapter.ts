19f670af0ab6e514ef3cfaf27df28ca970a345df
iohanalopes <111798386+iohanalopes@users.noreply.github.com>
2024-09-24 22:28:02 -0300

    Update adapter.ts

--- poo/grafite/adapter.ts
+++ poo/grafite/adapter.ts
@@ -1,18 +1,28 @@
+import { Grafite } from './grafite';
+import { Lapiseira } from './lapiseira';
+
 class Adapter {
+    lapiseira: Lapiseira;
+    
     constructor(thickness: number) {
+        this.lapiseira = new Lapiseira(thickness);
     }
 
-    insert(thickness: number, hardness: string, length: number) {
+    insert(thickness: number, hardness: string, size: number) {
+        let grafite = new Grafite(size, thickness, hardness);
+        this.lapiseira.insertGrafite(grafite);
     }
 
     remove() {
+        this.lapiseira.remove()
     }
 
     writePage() {
+        this.lapiseira.writePage();
     }
 
     toString() {
-        return "";
+        return this.lapiseira.toString();
     }
 }
 
